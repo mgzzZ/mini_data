120261a70df459307a65dd7e41585ac6b1c7e49d
mgzzz <zhangguoming@youzan.com>
2023-03-24 17:59:02 +0800

    feat:测试

--- mini_data_gen/lib/mini_data_gen.dart
+++ mini_data_gen/lib/mini_data_gen.dart
@@ -3,11 +3,11 @@ library mini_data_gen;
 import 'package:build/build.dart' show Builder, BuilderOptions;
 import 'package:mini_data_gen/src/copy_with_generator.dart';
 import 'package:mini_data_gen/src/settings.dart';
-import 'package:source_gen/source_gen.dart' show PartBuilder, SharedPartBuilder;
+import 'package:source_gen/source_gen.dart' show SharedPartBuilder;
 
 Builder copyWith(BuilderOptions config) {
-  return PartBuilder(
+  return SharedPartBuilder(
     [CopyWithGenerator(Settings.fromConfig(config.config))],
-    '.mini.dart',
+    'copyWith',
   );
 }
