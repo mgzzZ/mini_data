9ceb43c1d23eb96a6348cf96a4dc3902e736b8fe
mgzzz <zhangguoming@youzan.com>
2023-03-24 17:30:05 +0800

    feat:更改文件后缀

--- mini_data_gen/lib/mini_data_gen.dart
+++ mini_data_gen/lib/mini_data_gen.dart
@@ -3,11 +3,11 @@ library mini_data_gen;
 import 'package:build/build.dart' show Builder, BuilderOptions;
 import 'package:mini_data_gen/src/copy_with_generator.dart';
 import 'package:mini_data_gen/src/settings.dart';
-import 'package:source_gen/source_gen.dart' show SharedPartBuilder;
+import 'package:source_gen/source_gen.dart' show LibraryBuilder;
 
 Builder copyWith(BuilderOptions config) {
-  return SharedPartBuilder(
-    [CopyWithGenerator(Settings.fromConfig(config.config))],
-    'copyWith',
-  );
+  return LibraryBuilder(CopyWithGenerator(Settings.fromConfig(config.config)),
+      generatedExtension: '.mini.dart'
+      // 'copyWith',
+      );
 }
