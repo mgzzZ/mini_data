ce458a2a163e5ea6d90ca5177b90263e589ac70c
mgzzz <zhangguoming@youzan.com>
2023-04-23 15:59:40 +0800

    feat:test

---
+++ study/lib/student.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'student.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Student _$StudentFromJson(Map<String, dynamic> json) => Student()
+  ..name = json['name'] as String?
+  ..age = json['age'] as int?;
+
+Map<String, dynamic> _$StudentToJson(Student instance) => <String, dynamic>{
+      'name': instance.name,
+      'age': instance.age,
+    };
