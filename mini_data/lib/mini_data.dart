bf9d75436d1dac4f2167facb75a2bd096575935d
mgzzz <zhangguoming@youzan.com>
2023-04-26 14:57:42 +0800

    feat:test

--- mini_data/lib/mini_data.dart
+++ mini_data/lib/mini_data.dart
@@ -1,14 +1,10 @@
 library mini_data;
 
-import 'package:source_gen/source_gen.dart';
-
 class MiniDataConfig {
-  final bool generateCopyWith;
-  final bool generateMerge;
-
   const MiniDataConfig({
     required this.generateCopyWith,
     required this.generateMerge,
   });
-
+  final bool generateCopyWith;
+  final bool generateMerge;
 }
